148d8275683a5b01674cfa788670427ab776362b
Nguyen Ngoc Hien <ngochien.140604@gmail.com>
2026-05-20 19:59:05 +0700

    feat: configure allure report integration and CI pages pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,8 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never' }],
-    ['list']
+    ['list'],
+    ['allure-playwright', { detail: true, outputFolder: 'allure-results' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
